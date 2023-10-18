59a95cc4b83f212abd241a6500560b73137fd8ee
Warren Byron <wbyron@shoprite.co.za>
2023-10-18 18:13:40 +0100

    FoodItem data class - matches table

---
+++ src/main/kotlin/ie/setu/domain/FoodItem.kt
@@ -0,0 +1,23 @@
+package ie.setu.domain
+
+data class FoodItem (
+    var food_id: Int,
+    var name: String,
+    var calories: Int,
+    var carbohydrates: Double,
+    var proteins: Double,
+    var fats: Double,
+    var vitamins: Double,
+    var minerals: String
+)
+
+/*
+    food_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    calories INT NOT NULL,
+    carbohydrates DECIMAL(8, 2) NOT NULL,
+    proteins DECIMAL(8, 2) NOT NULL,
+    fats DECIMAL(8, 2) NOT NULL,
+    vitamins VARCHAR(255),
+    minerals VARCHAR(255)
+ */
