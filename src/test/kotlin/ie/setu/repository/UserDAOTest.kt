ac84508a0fc425ecccb390c211403c0eae003072
Warren Byron <wbyron@shoprite.co.za>
2023-10-23 17:23:48 +0100

    Week 6 - 04: More unit tests implemented

--- src/test/kotlin/ie/setu/repository/UserDAOTest.kt
+++ src/test/kotlin/ie/setu/repository/UserDAOTest.kt
@@ -1,24 +1,19 @@
 package ie.setu.repository
 
-import ie.setu.domain.User
 import ie.setu.domain.db.Users
 import ie.setu.domain.repository.UserDAO
-import ie.setu.helpers.nonExistingEmail
-//import ie.setu.helpers.populateUserTable
 import ie.setu.helpers.users
-//fimport org.junit.jupiter.api.assertE
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.junit.jupiter.api.*
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 //retrieving some test data from Fixtures
-val user1 = users[0]
-val user2 = users[1]
-val user3 = users[2]
+val user1 = users.get(0)
+val user2 = users.get(1)
+val user3 = users.get(2)
 
 class UserDAOTest {
 
@@ -37,11 +32,7 @@ class UserDAOTest {
         transaction {
 
             //Arrange - create and populate table with three users
-            SchemaUtils.create(Users)
-            val userDAO = UserDAO()
-            userDAO.save(user1)
-            userDAO.save(user2)
-            userDAO.save(user3)
+            val userDAO = populateUserTable()
 
             //Act & Assert
             assertEquals(3, userDAO.getAll().size)
@@ -49,5 +40,54 @@ class UserDAOTest {
             assertEquals(user2, userDAO.findById(user2.userId))
             assertEquals(user3, userDAO.findById(user3.userId))
         }
+    }
+
+    @Test
+    fun `getting all users from a populated table returns all rows`() {
+        transaction {
+
+            //Arrange - create and populate table with three users
+            val userDAO = populateUserTable()
+
+            //Act & Assert
+            assertEquals(3, userDAO.getAll().size)
+        }
+    }
+
+    @Test
+    fun `get user by id that doesn't exist, results in no user returned`() {
+        transaction {
+
+            //Arrange - create and populate table with three users
+            val userDAO = populateUserTable()
+
+            //Act & Assert
+            assertEquals(null, userDAO.findById(4))
+        }
+    }
+
+    @Test
+    fun `get user by id that exists, results in a correct user returned`() {
+        transaction {
+            //Arrange - create and populate table with three users
+            SchemaUtils.create(Users)
+            val userDAO = UserDAO()
+            userDAO.save(user1)
+            userDAO.save(user2)
+            userDAO.save(user3)
+
+            //Act & Assert
+            assertEquals(null, userDAO.findById(4))
+        }
+
+    }
+
+    internal fun populateUserTable(): UserDAO{
+        SchemaUtils.create(Users)
+        val userDAO = UserDAO()
+        userDAO.save(user1)
+        userDAO.save(user2)
+        userDAO.save(user3)
+        return userDAO
     }
 }
