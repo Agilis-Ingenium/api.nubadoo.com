7407f0d34079d50e6eb6a5362ab0ac9aae5d3a10
Warren Byron <wbyron@shoprite.co.za>
2023-10-28 18:49:13 +0100

    Added OpenAPI annotations

--- src/main/kotlin/ie/setu/controllers/FitnessGoalController.kt
+++ src/main/kotlin/ie/setu/controllers/FitnessGoalController.kt
@@ -4,6 +4,7 @@ import ie.setu.domain.FitnessGoal
 import ie.setu.domain.repository.FitnessGoalDAO
 import ie.setu.utils.jsonToObject
 import io.javalin.http.Context
+import io.javalin.openapi.*
 
 /**
  * Controller for managing fitness goals in the Health Tracker API.
@@ -18,6 +19,16 @@ object FitnessGoalController {
      *
      * @param ctx The Javalin context for handling HTTP requests.
      */
+    @OpenApi(
+        summary = "Get all fitness goals",
+        operationId = "getAllFitnessGoals",
+        tags = ["Fitness"],
+        responses = [
+            OpenApiResponse("200")
+        ],
+        path = "/v1/fitness-goals",
+        methods = [HttpMethod.GET]
+    )
     fun getAllFitnessGoals(ctx: Context) {
         val fitnessGoals = fitnessGoalDao.getAll()
         if (fitnessGoals.size != 0) {
@@ -34,6 +45,17 @@ object FitnessGoalController {
      *
      * @param ctx The Javalin context for handling HTTP requests.
      */
+    @OpenApi(
+        summary = "Get fitness goal by ID",
+        operationId = "getFitnessGoalByFitnessGoalId",
+        tags = ["Fitness"],
+        responses = [
+            OpenApiResponse("200", [OpenApiContent(FitnessGoal::class)]),
+            OpenApiResponse("404", [OpenApiContent(String::class)])
+        ],
+        path = "/v1/fitness-goals/{fitness-goal-id}",
+        methods = [HttpMethod.GET]
+    )
     fun getFitnessGoalByFitnessGoalId(ctx: Context) {
         val fitnessGoal = fitnessGoalDao.findById(ctx.pathParam("fitness-goal-id").toInt())
         if (fitnessGoal != null) {
@@ -50,6 +72,18 @@ object FitnessGoalController {
      *
      * @param ctx The Javalin context for handling HTTP requests.
      */
+    @OpenApi(
+        summary = "Add a new fitness goal",
+        operationId = "addFitnessGoal",
+        tags = ["Fitness"],
+        requestBody = OpenApiRequestBody([OpenApiContent(FitnessGoal::class)]),
+        responses = [
+            OpenApiResponse("200", [OpenApiContent(FitnessGoal::class)]),
+            OpenApiResponse("400", [OpenApiContent(String::class)])
+        ],
+        path = "/v1/fitness-goals",
+        methods = [HttpMethod.POST]
+    )
     fun addFitnessGoal(ctx: Context) {
         val fitnessGoal : FitnessGoal = jsonToObject(ctx.body())
         val fitnessGoalId = fitnessGoalDao.save(fitnessGoal)
@@ -65,6 +99,17 @@ object FitnessGoalController {
      *
      * @param ctx The Javalin context for handling HTTP requests.
      */
+    @OpenApi(
+        summary = "Delete fitness goal by ID",
+        operationId = "deleteFitnessGoal",
+        tags = ["Fitness"],
+        responses = [
+            OpenApiResponse("200", [OpenApiContent(FitnessGoal::class)]),
+            OpenApiResponse("404", [OpenApiContent(String::class)])
+        ],
+        path = "/v1/fitness-goals/{fitness-goal-id}",
+        methods = [HttpMethod.DELETE]
+    )
     fun deleteFitnessGoal(ctx: Context){
         if (fitnessGoalDao.delete(ctx.pathParam("fitness-goal-id").toInt()) != 0)
             ctx.status(204)
@@ -77,6 +122,18 @@ object FitnessGoalController {
      *
      * @param ctx The Javalin context for handling HTTP requests.
      */
+    @OpenApi(
+        summary = "Update fitness goal by ID",
+        operationId = "updateFitnessGoal",
+        tags = ["Fitness"],
+        responses = [
+            OpenApiResponse("204", [OpenApiContent(FitnessGoal::class)]),
+            OpenApiResponse("404", [OpenApiContent(String::class)])
+        ],
+        path = "/v1/fitness-goals/{fitness-goal-id}",
+        methods = [HttpMethod.PATCH],
+        requestBody = OpenApiRequestBody([OpenApiContent(FitnessGoal::class)])
+    )
     fun updateFitnessGoal(ctx: Context){
         val foundFitnessGoal : FitnessGoal = jsonToObject(ctx.body())
         if ((fitnessGoalDao.update(fitnessGoalId = ctx.pathParam("fitness-goal-id").toInt(), fitnessGoal=foundFitnessGoal)) != 0)
